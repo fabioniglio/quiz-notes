58bb7258cd2722912adda557f9d522048e8f3e20
Tiger Abrodi <tigerabrodi@gmail.com>
2025-03-02 17:33:50 +0100

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import { Toaster } from '@/components/ui/sonner'
 import { Analytics } from '@vercel/analytics/react'
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import './index.css'
 
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-      <Analytics />
-    </BrowserRouter>
+    <ConvexProvider client={convex}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+        <Analytics />
+      </BrowserRouter>
+    </ConvexProvider>
   </StrictMode>
 )
