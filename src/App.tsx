f8f22c157f693f7a1bc11910bc659faa8d4f10f8
Tiger Abrodi <tigerabrodi@gmail.com>
2025-03-08 19:56:58 +0100

    app refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import { Route, Routes } from 'react-router'
 import { AuthenticatedLayout } from './layouts/authenticated'
 import { ROUTES } from './lib/constants'
 import { LoginPage } from './pages/login'
-import { QuizDetail } from './pages/quiz-detail'
-import { QuizHome } from './pages/quiz-home'
-import { QuizResults } from './pages/quiz-results'
+import { QuizDetailPage } from './pages/quiz-detail'
+import { QuizHomePage } from './pages/quiz-home'
+import { QuizResultsPage } from './pages/quiz-results'
 
 function App() {
   return (
     <Routes>
       <Route path={ROUTES.login} element={<LoginPage />} />
       <Route element={<AuthenticatedLayout />}>
-        <Route path={ROUTES.quizHome} element={<QuizHome />} />
-        <Route path={ROUTES.quizDetail} element={<QuizDetail />} />
-        <Route path={ROUTES.quizDetailResults} element={<QuizResults />} />
+        <Route path={ROUTES.quizHome} element={<QuizHomePage />} />
+        <Route path={ROUTES.quizDetail} element={<QuizDetailPage />} />
+        <Route path={ROUTES.quizDetailResults} element={<QuizResultsPage />} />
       </Route>
     </Routes>
   )
