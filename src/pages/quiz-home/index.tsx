0d0342903c6d850a2e12ff16f6660655e1846523
Tiger Abrodi <tigerabrodi@gmail.com>
2025-03-09 16:27:31 +0100

    fix: quiz home max width

--- src/pages/quiz-home/index.tsx
+++ src/pages/quiz-home/index.tsx
@@ -9,7 +9,7 @@ export type QuizWithProgress = FunctionReturnType<
 
 export function QuizHomePage() {
   return (
-    <div className="mx-auto flex flex-col gap-10">
+    <div className="mx-auto flex max-w-7xl flex-col gap-10">
       <div>
         <h1 className="mb-2 text-3xl font-bold">Create a New Quiz</h1>
         <p className="text-muted-foreground">
