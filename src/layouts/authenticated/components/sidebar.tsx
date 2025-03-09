d891025631a51cd24d626e9f481458fe6f8cd76b
Tiger Abrodi <tigerabrodi@gmail.com>
2025-03-09 17:54:43 +0100

    perf: prefetch quiz sidebar items

--- src/layouts/authenticated/components/sidebar.tsx
+++ src/layouts/authenticated/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
+import { usePrefetchQuery } from '@/hooks/use-prefetch-query'
 import { ROUTES } from '@/lib/constants'
 import { cn } from '@/lib/utils'
 import { useAuthActions } from '@convex-dev/auth/react'
@@ -17,10 +18,33 @@ function QuizLinkItem({ quiz }: { quiz: Doc<'quizzes'> }) {
     ? generatePath(ROUTES.quizDetailResults, { quizId: quiz._id })
     : generatePath(ROUTES.quizDetail, { quizId: quiz._id })
 
+  const prefetchResultsQuery = usePrefetchQuery(
+    api.quizzes.queries.getQuizResults,
+    {
+      quizId: quiz._id,
+    }
+  )
+
+  const prefetchQuizDetailQuery = usePrefetchQuery(
+    api.quizzes.queries.getQuizById,
+    {
+      id: quiz._id,
+    }
+  )
+
+  const handlePrefetch = () => {
+    if (quiz.isCompleted) {
+      prefetchResultsQuery()
+    } else {
+      prefetchQuizDetailQuery()
+    }
+  }
+
   return (
     <Link
       key={quiz._id}
       to={quizPath}
+      onMouseEnter={handlePrefetch}
       className={cn('rounded-md px-3 py-2 text-sm', {
         'bg-muted font-medium': pathname === quizPath,
         'hover:bg-muted/50': pathname !== quizPath,
@@ -35,13 +59,21 @@ export function Sidebar() {
   const quizzes = useQuery(api.quizzes.queries.getAllQuizzesByUserId)
   const [isSettingsDialogOpen, setIsSettingsDialogOpen] = useState(false)
 
+  const prefetchQuizHomeQuery = usePrefetchQuery(
+    api.quizzes.queries.listQuizzesWithProgress
+  )
+
+  const handlePrefetch = () => {
+    prefetchQuizHomeQuery()
+  }
+
   const { signOut } = useAuthActions()
 
   return (
     <>
       <div className="bg-background sticky top-0 left-0 z-50 flex h-screen w-64 shrink-0 flex-col border-r">
         <div className="p-5">
-          <Link to={ROUTES.quizHome}>
+          <Link to={ROUTES.quizHome} onMouseEnter={handlePrefetch}>
             <div className="flex items-center gap-2">
               <BookOpen className="h-6 w-6" />
               <h1 className="text-xl font-bold">Notes to Quiz</h1>
